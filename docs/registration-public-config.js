0dfd51f043fd7b6d16f8e885f332e8472e79c2ff
Hana <hana31923@users.noreply.github.com>
2026-09-02 01:04:02 +0800

    Connect registration webhook

--- docs/registration-public-config.js
+++ docs/registration-public-config.js
@@ -1,5 +1,5 @@
 window.HANA_REGISTRATION_CONFIG = {
   googleSheetId: "15RUfmIkwgZwlFfdXJ9LXaSxQZwarSdVCX9cIk0j2qFM",
   googleSheetUrl: "https://docs.google.com/spreadsheets/d/15RUfmIkwgZwlFfdXJ9LXaSxQZwarSdVCX9cIk0j2qFM/edit",
-  webhookUrl: "",
+  webhookUrl: "https://script.google.com/macros/s/AKfycbxIGlPg5ddzWWYQFjRvdecOxbuMbsZ76y6VSpb_AKr12CDwfVSFOzwyt5KkfsydcyFd/exec",
 };
